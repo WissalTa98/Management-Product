1acd8ae1d6b901cde990877f1108694980e658f6
WissalTa98 <wissal.talbi@esprit.tn>
2023-10-24 10:26:44 +0100

    Js in Blazor

---
+++ wwwroot/js/common.js
@@ -0,0 +1,24 @@
+﻿window.ShowToastr = (type, message) => {
+    if (type === "success") {
+        toastr.success(message, "Operation Successful", { timeOut: 5000 });
+    }
+    if (type === "error") {
+        toastr.error(message, "Operation Failed", { timeOut: 5000 });
+    }
+}
+
+window.ShowSweetAlert = (type) => {
+    if (type === "success") {
+        Swal.fire(
+            'Success Notification!',
+            'Task completed successfully!',
+            'success'
+        );
+    } else if (type === "error") {
+        Swal.fire(
+            'Error Notification!',
+            'Task failed!',
+            'error'
+        );
+    }
+}
