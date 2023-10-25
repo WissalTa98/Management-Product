cdae3fcc502e9ae5669e35989d6a830a742367dd
WissalTa98 <wissal.talbi@esprit.tn>
2023-10-25 17:16:05 +0100

    CRUD Product with Syncfusion Components

--- Program.cs
+++ Program.cs
@@ -2,19 +2,27 @@ using app_Business.Repository;
 using app_Business.Repository.IRepository;
 using app_DataAccess.Data;
 using app_Server.Data;
+using app_Server.Service;
+using app_Server.Service.IService;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.EntityFrameworkCore;
+using Syncfusion.Blazor;
+
+Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Ngo9BigBOggjHTQxAR8/V1NHaF5cXmVCf1JpR2dGfV5yd0VDal5WTnRbUiweQnxTdEZiWX1bcXZVRWFVVEJ+Xw==");
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddSyncfusionBlazor();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 	options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+builder.Services.AddScoped<IFileUpload, FileUpload>();
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 var app = builder.Build();
 
